7c1b07ebbe1e2cc579a98cf660c234532d2ccbbb
Camilla Hollmann <camilla.hollmann@pucpr.edu.br>
2025-08-18 22:11:01 -0300

    starting something new

--- 2025_08_18/soma_2.asm
+++ 2025_08_18/soma_2.asm
@@ -8,14 +8,22 @@
 soma: 
     pushq %rbp
     movq %rsp, $rbp
-    subq $8, %rsp
+    subq $8, %rsp ## P alocar espaço p var local
     movq 16(r%bp), %rax
     movq 24(%rbp), %rbx
+    aadq %rbx, %rax
+    movq %rax, -8(%rbp)
+    addq $8, %rsp
+    popq %rbp
+    ret
 
 _start:
 	movq $4, A
 	movq $5, B
     pushq A
     pushq B
-	
 	call soma
+    addq $16, %rsp
+    movq %rax, %rdi
+    movq $60, %rax
+    syscall
